162005a26063179d37b52fe74b395c209cd7f5dc
DIPANSHU66 <dipanshu682004@gmail.com>
2025-02-18 00:10:47 +0530

    Deploying latest changes

--- frontend/src/utils/Constant.js
+++ frontend/src/utils/Constant.js
@@ -1,8 +1,8 @@
 export const USER_API_END_POINT =
-  "https://fullstack-jobquest-bydipanshu.onrender.com/login/api/v1/user";
+  "https://fullstack-jobquest-bydipanshu.onrender.com/api/v1/user";
 export const Job_API_END_POINT =
-  "https://fullstack-jobquest-bydipanshu.onrender.com/login/api/v1/jobs";
+  "https://fullstack-jobquest-bydipanshu.onrender.com/api/v1/jobs";
 export const APPLYCATION_API_END_POINT =
-  "https://fullstack-jobquest-bydipanshu.onrender.com/login/api/v1/application";
+  "https://fullstack-jobquest-bydipanshu.onrender.com/api/v1/application";
 export const COMPANY_API_END_POINT =
-  "https://fullstack-jobquest-bydipanshu.onrender.com/login/api/v1/company";
+  "https://fullstack-jobquest-bydipanshu.onrender.com/api/v1/company";
