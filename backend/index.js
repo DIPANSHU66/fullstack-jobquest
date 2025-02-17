968e486e1026de5c76f2550565a573d7a6ae8d05
DIPANSHU66 <dipanshu682004@gmail.com>
2025-02-17 23:48:55 +0530

    Deploying latest changes

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsoptions = {
-  origin: "http://localhost:5173",
+  origin: process.env.URL,
   credentials: true,
 };
 app.use(cors(corsoptions));
--- frontend/src/utils/Constant.js
+++ frontend/src/utils/Constant.js
@@ -1,4 +1,8 @@
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
-export const Job_API_END_POINT = "http://localhost:8000/api/v1/jobs";
-export const APPLYCATION_API_END_POINT = "http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
+export const USER_API_END_POINT =
+  "https://fullstack-jobquest-bydipanshu.onrender.com/login/v1/user";
+export const Job_API_END_POINT =
+  "https://fullstack-jobquest-bydipanshu.onrender.com/login/api/v1/jobs";
+export const APPLYCATION_API_END_POINT =
+  "https://fullstack-jobquest-bydipanshu.onrender.com/login/api/v1/application";
+export const COMPANY_API_END_POINT =
+  "https://fullstack-jobquest-bydipanshu.onrender.com/login/api/v1/company";
